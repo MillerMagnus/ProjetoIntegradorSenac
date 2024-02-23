ed598aefce21e400d47ced395a9c855d60a29d24
MillerMagnus <millermagnus@outlook.com>
2024-02-22 22:05:44 -0300

    primeiro commit

---
+++ catalogo-pi/src/pages/index.js
@@ -0,0 +1,22 @@
+export default function Home() {
+
+  const Teste01 = () => {
+    console.log("chegou aqui")
+  }
+
+  return (
+    <div className="row row-cols-1 row-cols-md-3 g-4">
+  <div className="col">
+    <div className="card h-100">
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+
+  
+</div>
+  );
+}
